f37e7c5f8ab626015bbaa03a941dc77d07605716
Hiral <hiralg@uw.edu>
2020-05-21 15:40:26 -0700

    Adding Task1 and Initial Webpage.

---
+++ Test.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    print("result: ", x+y)
+    return
+
+add(4,5)
+
---
+++ Test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int x, int y){
+    return x+y;
+}
+
+int main(){
+
+    cout<<"result: "<<add(4,5);
+    return 1;
+}
